34093e5306df8e0412fd3c578e0c7f41f6790748
Tom McLaughlin <tom@codedown.io>
2023-02-12 00:50:34 -0800

    Prune flake inputs and add mkScreenshotter

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
   description = "CodeDown Go Screenshotter";
 
-  inputs.gitignore = {
-    url = "github:hercules-ci/gitignore.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, gitignore, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     # flake-utils.lib.eachDefaultSystem (system:
     # flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
@@ -21,6 +17,13 @@
         packages = (rec {
           default = pkgs.callPackage ./. {};
           dynamic = pkgs.callPackage ./. { static = false; };
+
+          mkScreenshotter = { chromePath }: with pkgs; runCommand "codedown-screenshotter-go" { buildInputs = [makeWrapper]; } ''
+            mkdir -p $out/bin
+
+            makeWrapper ${default}/bin/codedown-screenshotter "$out/bin/codedown-screenshotter" \
+              --add-flags chrome-path "${chromePath}"
+          '';
         });
       });
 }
