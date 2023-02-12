cf2b29a75997e47b5f99a445fb4bbd012b6d0863
Tom McLaughlin <tom@codedown.io>
2023-02-12 00:10:22 -0800

    Add cookies

--- main.go
+++ main.go
@@ -8,18 +8,30 @@ import (
 	"os"
 
 	"github.com/chromedp/chromedp"
+	"github.com/chromedp/cdproto/network"
 )
 
 func main() {
+	url := flag.String("url", "", "URL to screenshot")
+	chromePath := flag.String("chrome-path", "", "Path to chrome or headless-shell executable")
+
 	width := flag.Int("width", 850, "Viewport width")
 	height := flag.Int("height", 1000, "Viewport height")
+
 	quality := flag.Int("quality", 90, "PNG quality (0-100)")
-	chromePath := flag.String("chrome-path", "", "Path to chrome or headless-shell executable")
-	url := flag.String("url", "", "URL to screenshot")
+
+	cookieName := flag.String("cookieName", "", "Cookie name")
+	cookieValue := flag.String("cookieValue", "", "Cookie value")
+
 	flag.Parse()
 
 	if *url == "" {
 		log.Fatal("-url is required")
+	}
+
+	headers := make(map[string]interface{})
+	if *cookieName != "" && *cookieValue != "" {
+		headers["Cookie"] = *cookieName + "=" + *cookieValue
 	}
 
 	options := []chromedp.ExecAllocatorOption{}
@@ -36,19 +48,30 @@ func main() {
 	defer cancel()
 
 	var buf []byte
-	if err := chromedp.Run(ctx, fullScreenshot(*url, *quality, &buf)); err != nil {
+	if err := chromedp.Run(ctx, fullScreenshot(*url, *quality, &buf, &headers)); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+	if err := os.WriteFile("screenshot.png", buf, 0o644); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Wrote fullScreenshot.png")
+	log.Printf("Wrote screenshot.png")
 }
 
-func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.FullScreenshot(res, quality),
+func fullScreenshot(
+	urlstr string,
+	quality int,
+	res *[]byte,
+	headers *map[string]interface{},
+) chromedp.Tasks {
+	var actions chromedp.Tasks
+
+	if len(*headers) > 0 {
+		actions = append(actions, network.Enable(), network.SetExtraHTTPHeaders(network.Headers(*headers)))
 	}
+
+	actions = append(actions, chromedp.Navigate(urlstr))
+	actions = append(actions, chromedp.FullScreenshot(res, quality))
+
+	return actions
 }
