43d2bf634382b5e72a1646dd2594eed3f4b5ff99
Tom McLaughlin <tom@codedown.io>
2023-02-11 23:55:41 -0800

    Use flags

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"os"
 
@@ -10,25 +11,32 @@ import (
 )
 
 func main() {
-	var url string = `https://brank.as/`
-	var width int = 850
-	var height int = 850
-	var chromePath string = "/usr/bin/google-chrome";
+	width := flag.Int("width", 850, "Viewport width")
+	height := flag.Int("height", 1000, "Viewport height")
+	quality := flag.Int("quality", 90, "PNG quality (0-100)")
+	chromePath := flag.String("chrome-path", "", "Path to chrome or headless-shell executable")
+	url := flag.String("url", "", "URL to screenshot")
+	flag.Parse()
+
+	if *url == "" {
+		log.Fatal("-url is required")
+	}
 
 	options := []chromedp.ExecAllocatorOption{}
 	options = append(options, chromedp.DefaultExecAllocatorOptions[:]...)
 	options = append(options, chromedp.DisableGPU)
-	options = append(options, chromedp.WindowSize(width, height))
-	options = append(options, chromedp.ExecPath(chromePath))
+	options = append(options, chromedp.WindowSize(*width, *height))
+	if *chromePath != "" {
+		options = append(options, chromedp.ExecPath(*chromePath))
+	}
 
 	actx, acancel := chromedp.NewExecAllocator(context.Background(), options...)
 	defer acancel()
 	ctx, cancel := chromedp.NewContext(actx)
 	defer cancel()
 
-	var quality int = 90
 	var buf []byte
-	if err := chromedp.Run(ctx, fullScreenshot(url, quality, &buf)); err != nil {
+	if err := chromedp.Run(ctx, fullScreenshot(*url, *quality, &buf)); err != nil {
 		log.Fatal(err)
 	}
 	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
