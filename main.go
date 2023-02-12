e8925042e7e95ae6880ca6a04dd0016596db9e88
Tom McLaughlin <tom@codedown.io>
2023-02-11 22:45:43 -0800

    Simplify main.go

--- main.go
+++ main.go
@@ -1,5 +1,4 @@
-// Command screenshot is a chromedp example demonstrating how to take a
-// screenshot of a specific element and of the entire browser viewport.
+
 package main
 
 import (
@@ -11,45 +10,24 @@ import (
 )
 
 func main() {
-	// create context
 	ctx, cancel := chromedp.NewContext(
 		context.Background(),
 		// chromedp.WithDebugf(log.Printf),
 	)
 	defer cancel()
 
-	// capture screenshot of an element
+	var quality int = 90;
 	var buf []byte
-	if err := chromedp.Run(ctx, elementScreenshot(`https://pkg.go.dev/`, `img.Homepage-logo`, &buf)); err != nil {
-		log.Fatal(err)
-	}
-	if err := os.WriteFile("elementScreenshot.png", buf, 0o644); err != nil {
-		log.Fatal(err)
-	}
-
-	// capture entire browser viewport, returning png with quality=90
-	if err := chromedp.Run(ctx, fullScreenshot(`https://brank.as/`, 90, &buf)); err != nil {
+	if err := chromedp.Run(ctx, fullScreenshot(`https://brank.as/`, quality, &buf)); err != nil {
 		log.Fatal(err)
 	}
 	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("wrote elementScreenshot.png and fullScreenshot.png")
+	log.Printf("Wrote fullScreenshot.png")
 }
 
-// elementScreenshot takes a screenshot of a specific element.
-func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.Screenshot(sel, res, chromedp.NodeVisible),
-	}
-}
-
-// fullScreenshot takes a screenshot of the entire browser viewport.
-//
-// Note: chromedp.FullScreenshot overrides the device's emulation settings. Use
-// device.Reset to reset the emulation and viewport settings.
 func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(urlstr),
